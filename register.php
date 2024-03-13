47a69fd257868573e5136557059fe8a92e53ead0
YeonSung <150204668+koinoniays2@users.noreply.github.com>
2024-03-13 11:13:49 +0900

    회원등록

--- register.php
+++ register.php
@@ -11,15 +11,19 @@
     // $address_option = $_POST["address_option"];
     $address = "(".$address_postcode.")".$address_.$address_detail/*."(".$address_option.")"*/;
   
-    $regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
+    $regist_day = date("Y-m-d H:i"); // 현재의 '년-월-일-시-분'을 저장
 
     $con = mysqli_connect("localhost", "admin", "admin", "dogether");
 
-	$sql = "INSERT INTO users(id, password, password_check, username, email, mobile_tel, regist_day, address) ";
-	$sql .= "VALUES('$id', '$password', '$password_check', '$name', '$email', '$mobile_tel', $regist_day, '$address')";
+	$sql = "INSERT INTO users(id, password, password_check, username, email, mobile_tel, address) ";
+	$sql .= "VALUES('$id', '$password', '$password_check', '$name', '$email', '$mobile_tel', '$address')";
 
-	mysqli_query($con, $sql);  // $sql 에 저장된 명령 실행
-    mysqli_close($con);     
-
-    echo json_encode(array('success' => true));
+    $result = mysqli_query($con, $sql); // $sql 에 저장된 명령 실행
+    mysqli_close($con); 
+        
+    if (!$result) {
+        echo json_encode(array('success' => false, 'message' => '회원가입에 실패했습니다.'));
+    } else {
+        echo json_encode(array('success' => true));
+    }
 ?>
