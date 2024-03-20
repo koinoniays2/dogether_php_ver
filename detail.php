c834e3cb0e60ea9a2f9daf3a46cf8bff569a0efc
sikkkkkw <gksmsla92@naver.com>
2024-03-20 16:40:21 +0900

    데이터받기

--- detail.php
+++ detail.php
@@ -52,29 +52,22 @@
       <div class="category"></div>
       <!-- map -->
       <div class="map_wrap">
-        <div id="map" style="
-              width: 100%;
-              height: 100%;
-              position: relative;
-              overflow: hidden;
-            "></div>
+    <div id="map" style="width:100%;height:100%;position:relative;overflow:hidden;"></div>
 
-        <div id="menu_wrap" class="bg_white">
-          <div class="option">
+    <div id="menu_wrap" class="bg_white">
+        <div class="option">
             <div>
-              <form id="search_form" onsubmit="searchPlaces(); return false;">
-                <!-- 키워드 : -->
-                <input type="text" value="애견동반" id="keyword" size="15" />
-                <button type="button"><i class="fa-solid fa-magnifying-glass" style="color: #808080;"></i></button>
-              </form>
+                <form onsubmit="searchPlaces(); return false;">
+                    키워드 : <input type="text" value="이태원 맛집" id="keyword" size="15"> 
+                    <button type="submit">검색하기</button> 
+                </form>
             </div>
-          </div>
-          <hr />
-          <ul id="placesList"></ul>
-          <div id="pagination"></div>
-
         </div>
-      </div>
+        <hr>
+        <ul id="placesList"></ul>
+        <div id="pagination"></div>
+    </div>
+</div>
       <!-- js 렌더링 -->
       <div class="content"></div>
       <!-- 페이지네이션 -->
--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
+// 마커를 담을 배열입니다
 var markers = [];
 
 var mapContainer = document.getElementById("map"), // 지도를 표시할 div
@@ -16,11 +17,11 @@ var ps = new kakao.maps.services.Places();
 var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
 
 // 키워드로 장소를 검색합니다
-// searchPlaces();
+searchPlaces();
 
 // 키워드 검색을 요청하는 함수입니다
-export default function searchPlaces(keyword) {
-  keyword = document.getElementById("keyword").value;
+function searchPlaces() {
+  var keyword = document.getElementById("keyword").value;
 
   if (!keyword.replace(/^\s+|\s+$/g, "")) {
     alert("키워드를 입력해주세요!");
@@ -30,6 +31,7 @@ export default function searchPlaces(keyword) {
   // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
   ps.keywordSearch(keyword, placesSearchCB);
 }
+
 // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
 function placesSearchCB(data, status, pagination) {
   if (status === kakao.maps.services.Status.OK) {
