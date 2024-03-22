855a23f22c75c23b02b54322b9eb3cd2014ec076
YeonSung <150204668+koinoniays2@users.noreply.github.com>
2024-03-22 09:45:40 +0900

    footer 수정

--- footer.php
+++ footer.php
@@ -6,23 +6,21 @@
 <body>
   <div class="footer_box">
     <div class="footer_con1">
-
-      <img src="./images/logo2.png" alt="dogether_logo" />
-
+      <a href="#">
+        <img class="logo_img" src="./images/logo2.png" alt="dogether_logo" />
+      </a>
       <div class="footer_service">
         <div class="service_info">
           <p><i class="fa-solid fa-headset fa-lg" style="color: #14471e; margin-right:10px;"></i>050-123-4567</p>
           <p><i class="fa-brands fa-instagram fa-xl" style="color: #01471e; margin-right:10px;"></i>todays_ondog</p>
-        </div>
-        <div class="service_time">
-          <p>10:00 ~ 17:00</p>
-          <p>주말, 공휴일 휴무</p>
+          <p><i class="fa-regular fa-clock fa-lg" style="color: #01471e; margin-right:10px;"></i>10:00 ~ 17:00</p>
+          <p><i class="fa-solid fa-shop-slash fa-lg" style="color: #01471e; margin-right:10px;"></i>주말, 공휴일 휴무</p>
         </div>
       </div>
     </div>
-    <div class="footer_con2">
+    <!-- <div class="footer_con2">
       <p class="footer_text">㈜ DOGETHER는 대표 최연성과 구성원들이 반려동물과 함께하는 건강한 일상을 위해 만든 사이트입니다. </p>
-    </div>
+    </div> -->
   </div>
 
 
