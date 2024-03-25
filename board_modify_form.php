c7d8b02367587c7ff2b960de2699c6d423af3ef3
etoilebonbon <violetjardin@daum.net>
2024-03-25 16:14:39 +0900

    게시판:board modify form(line height)

--- board_modify_form.php
+++ board_modify_form.php
@@ -50,15 +50,15 @@
 	    <form  name="board_form" method="post" action="board_modify.php?num=<?=$number?>&page=<?=$page?>" enctype="multipart/form-data">
 	    	 <ul id="board_form">
 				<li>
-					<span class="col1">이름 : </span>
+					<span class="col1"><b>이름 :</b> </span>
 					<span class="col2" id="testname"><?=$name?></span>
 				</li>		
 	    		<li>
-	    			<span class="col1">제목 : </span>
+	    			<span class="col1"><b>제목 :</b></span>
 	    			<span class="col2"><input name="subject" type="text" value="<?=$subject?>"></span>
 	    		</li>	    	
 	    		<li id="text_area">	
-	    			<span class="col1">내용 : </span>
+	    			<span class="col1"><b>내용 :</b> </span>
 	    			<span class="col2">
 	    				<textarea name="content"><?=$content?></textarea>
 	    			</span>
