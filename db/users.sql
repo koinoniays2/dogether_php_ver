57b3b807dababc12e5ce6404b2a05e950c7a2af2
YeonSung <150204668+koinoniays2@users.noreply.github.com>
2024-03-25 10:31:57 +0900

    db수정

--- db/users.sql
+++ db/users.sql
@@ -1,11 +1,12 @@
 CREATE TABLE users (
-id VARCHAR(20) PRIMARY KEY, -- 아이디
-password VARCHAR(20) NOT NULL, -- 비밀번호
-password_check VARCHAR(20) NOT NULL, -- 비밀번호 확인
+id VARCHAR(100) PRIMARY KEY, -- 아이디
+password VARCHAR(255) NOT NULL, -- 비밀번호
+password_check VARCHAR(255) NOT NULL, -- 비밀번호 확인
 name VARCHAR(255) NOT NULL, -- 이름
 email VARCHAR(255) NOT NULL, -- 이메일
 mobile_tel VARCHAR(20) NOT NULL, -- 휴대폰 번호
 address TEXT NOT NULL, -- 주소
+address_detail TEXT,
 login_div VARCHAR(20) -- 가입 유형
 );
 
