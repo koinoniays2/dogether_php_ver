95619302480318a7a2d1259da6a39c5232a0f24e
YeonSung <150204668+koinoniays2@users.noreply.github.com>
2024-03-25 10:53:12 +0900

    콘솔오류 수정

--- js/detail.js
+++ js/detail.js
@@ -11,25 +11,25 @@
 //   }, 6000); // 3초 후 + 3초 후
 // });
 
-document.addEventListener("DOMContentLoaded", function () {
-  // 페이지가 로드될 때 실행되는 함수
+// document.addEventListener("DOMContentLoaded", function () {
+//   // 페이지가 로드될 때 실행되는 함수
 
-  // 초기에 intro-box1을 서서히 나타나게 설정
-  let introBox1 = document.getElementById("intro-box1");
-  introBox1.style.transition = "opacity 1s ease-in-out";
-  introBox1.style.opacity = "1";
+//   // 초기에 intro-box1을 서서히 나타나게 설정
+//   let introBox1 = document.getElementById("intro-box1");
+//   introBox1.style.transition = "opacity 1s ease-in-out";
+//   introBox1.style.opacity = "1";
 
-  setTimeout(function () {
+//   setTimeout(function () {
     
 
-    // 3초 후에 display: none;으로 변경
-    setTimeout(function () {
-      // 1초 뒤에 intro-box2를 서서히 나타나게 설정
-      let introBox2 = document.getElementById("intro-box2");
-      introBox2.style.display = "block"; // 또는 "block" 등을 사용하여 적절한 디스플레이 속성을 설정
-      introBox2.style.visibility = "visible"; // visibility를 visible로 변경
-      introBox2.style.transition = "opacity 1s ease-in-out";
-      introBox2.style.opacity = "1";
-    }, 1000);
-  }, 1000);
-});
+//     // 3초 후에 display: none;으로 변경
+//     setTimeout(function () {
+//       // 1초 뒤에 intro-box2를 서서히 나타나게 설정
+//       let introBox2 = document.getElementById("intro-box2");
+//       introBox2.style.display = "block"; // 또는 "block" 등을 사용하여 적절한 디스플레이 속성을 설정
+//       introBox2.style.visibility = "visible"; // visibility를 visible로 변경
+//       introBox2.style.transition = "opacity 1s ease-in-out";
+//       introBox2.style.opacity = "1";
+//     }, 1000);
+//   }, 1000);
+// });
