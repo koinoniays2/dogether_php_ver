5338b5eba1c241cc91e7d4b1c33b97ef381cf637
etoilebonbon <violetjardin@daum.net>
2024-03-25 13:58:38 +0900

    게시판

--- db/board.sql
+++ db/board.sql
@@ -1,9 +1,9 @@
 create table board (
    num int not null auto_increment,
    id varchar(255),
-   name varchar(255) not null,
-   subject text not null,
-   content text not null,        
+   name varchar(255),
+   subject text,
+   content text,        
    regist_day char(20) not null,
    primary key(num)
 );
